161ab66bba84e31c3caadabfa216b96201d23854
Romulo Ribeiro Moreira <romulo.moreira@gmail.com>
2025-12-18 21:44:21 -0300

    added REMOVEUSER endpoint

--- src/routers/usuario.py
+++ src/routers/usuario.py
@@ -28,7 +28,7 @@ def get_users():
 
 
 # Rota de cadastrar usuário
-@router.post("/usuarios/cadastro", summary="Cadastrar usuário", description="Cadastra um usuário no banco de dados.",
+@router.post("/usuarios/cadastrar", summary="Cadastrar usuário", description="Cadastra um usuário no banco de dados.",
       responses = {
           201: {"description": "Usuário cadastrado com sucesso."},
           400: {"description": "Bad request: E-mail já utilizado em outro cadastro"},
@@ -48,7 +48,7 @@ def create_user(usuario: Usuario):
                 session.add(usuario) 
                 session.commit()
                 session.refresh(usuario)
-                logger.info(f'Usuário ${usuario.nome} cadastrado com sucesso')
+                logger.info(f'Usuário {usuario.nome} cadastrado com sucesso')
                 return JSONResponse(status_code=201, content={"message": "Usuário cadastrado com sucesso"})
     except IntegrityError as e:
         raise HTTPException(
@@ -76,3 +76,22 @@ def find_user(email_id: str):
             return usuario
     
 
+# Rota de deletar usuário
+@router.delete("/usuarios/remover/{email_id}", summary="Remover usuário", description="Remove um usuário do banco de dados pelo seu e-mail.",
+        responses = {
+            200: {"description": "Usuário deletado com sucesso"},
+            404: {"description": "Usuário não localizado"}
+} )
+def delete_user(email_id: str):
+    """ Rota para deletar usuário. Insira o e-mail do usuário que deseja deletar. """
+    with Session(engine) as session:
+        statement = select(Usuario).where(Usuario.email == email_id)
+        results = session.exec(statement)
+        usuario = results.first()
+        if not usuario:
+            return JSONResponse(status_code=404, content={"message": "Usuário não localizado"})
+        else:
+            session.delete(usuario)
+            session.commit()
+            logger.info(f'Usuário {usuario.nome} deletado com sucesso')
+            return JSONResponse(status_code=200, content={"message": "Usuário removido com sucesso"})
